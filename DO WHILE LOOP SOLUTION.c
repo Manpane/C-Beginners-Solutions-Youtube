0f6c50f18712cf718083f5bc3f24ad1cc3dbecbd
Manpane <75156544+Manpane@users.noreply.github.com>
2022-03-23 12:05:54 +0545

    Update DO WHILE LOOP SOLUTION.c

--- DO WHILE LOOP SOLUTION.c
+++ DO WHILE LOOP SOLUTION.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
-  int num = 98;
+  int num = 98; 
   do{
-    printf("%d \n",num);  // \n for new line
-    num = num-2;
-  }while(num>0);
+    printf("%d \n",num);  // printing value of num and \n for new line
+    num = num-2; // decreasing num by 2 (98-2 = 96)
+  }while(num>0);// until the num is greater than 0
 }
